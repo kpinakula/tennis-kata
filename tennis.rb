cd4a4c86983b4ce29194927fca9371ac1154b898
Rosie Hamer <rosiehamer@Rosies-MBP.home>
2022-06-14 11:49:46 +0100

    Generalise score calculator logic

--- tennis.rb
+++ tennis.rb
@@ -1,13 +1,17 @@
 def score_calculator(points) 
-    player1_score = "love"
-    player2_score = "love"
-    if points.count('1') == 1
-        player1_score = "15"
+    player1_score = ""
+    player2_score = ""
+    score_list = ["love", "15", "30", "40"]
+
+    score_list.each_with_index do |score, index|
+        if index == points.count('1')
+            player1_score = score
+        end
+        if points.count('2') == index
+            player2_score = score
+        end
     end
-    # if points.count('2') == 0
-    #     player2_score = "love"
-    # end
-
+    
     scoreboard = player1_score + ":" + player2_score
-
 end
+# score_calculator("121")
